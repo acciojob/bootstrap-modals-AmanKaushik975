c8ce7c1b8381ed2e136b8fe41608ed6d451b6d5c
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-05-03 00:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+const openModalButton = document.getElementById('open-modal');
+const newtonModal = new bootstrap.Modal(document.getElementById('newton-modal'));
+
+openModalButton.addEventListener('click', () => {
+  newtonModal.show();
+});
